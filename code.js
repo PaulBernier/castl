31852d6b1dc0e8e474faf0cfa79330ee24c0ab7c
Paul <bernier.pja@gmail.com>
2014-09-23 18:40:09 +0200

    update package.json to version 1.1.8

--- code.js
+++ code.js
@@ -1,11 +1,6 @@
-// CHECK#1
-var object = {valueOf: function() {return "1"}, toString: function() {return 0}};
-if (Number(object) !== 1) {
-  $ERROR('#1: var object = {valueOf: function() {return "1"}, toString: function() {return 0}}; Number(object) === 1. Actual: ' + (Number(object)));
+// This is an example to compile
+function renderCurve() {
+for(var a = 1, b = 10; a * b; a++, b--)
+console.log(new Array(a * b).join('*'));
 }
-
-// CHECK#2
-var object = {valueOf: function() {return {}}, toString: function() {return "0"}};
-if (Number(object) !== 0) {
-  $ERROR('#2: var object = {valueOf: function() {return {}}, toString: function() {return "0"}}; Number(object) === 0. Actual: ' + (Number(object)));
-}
+renderCurve();
